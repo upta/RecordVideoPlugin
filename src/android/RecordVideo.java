3b7a8a136f5fc953748c098b5f35f0b0d5a806e5
Brian Uptagrafft <upta@outlook.com>
2016-05-10 15:57:02 -0700

    options

--- src/android/RecordVideo.java
+++ src/android/RecordVideo.java
@@ -96,11 +96,12 @@ public class RecordVideo extends CordovaPlugin {
                 new MaterialCamera(cordova.getActivity())
                         //.saveDir(saveDir)
                         .showPortraitWarning(false)
+                        .defaultToFrontFacing(false) 
                         .autoSubmit(true)
                         .allowRetry(false)
                         .videoFrameRate(120)
                         .videoPreferredHeight(720)
-                        .countdownSeconds(30f)
+                        .countdownSeconds(6f)
                         .defaultToFrontFacing(true)
                         .start(1);
     }
